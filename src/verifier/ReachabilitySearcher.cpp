52012488d6026b5bdefee3fa102e0542e6a4b913
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-11-27 15:38:34 +0100

    Added ReachabilitySearcher class

---
+++ src/verifier/ReachabilitySearcher.cpp
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2020 Asger Gitz-Johansen
+
+   This file is part of mave.
+
+    mave is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    mave is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mave.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "ReachabilitySearcher.h"
+
+bool IsQuerySatisfiedHelper(Query& query, const TTA& state) {
+    switch (query.root.type) {
+        case NodeType_t::Location: return state.GetCurrentLocations().find(query.root.token) != state.GetCurrentLocations().end();
+        // TODO: ↓ What about tock changes? - Maybe NextTickStates should be done via a successor generator class
+        case NodeType_t::Deadlock: return state.GetNextTickStates().empty(); //// Expensive, fix it
+        case NodeType_t::LogicAnd: return IsQuerySatisfiedHelper(query.children[0], state) && IsQuerySatisfiedHelper(query.children[1], state);
+        case NodeType_t::LogicOr:  return IsQuerySatisfiedHelper(query.children[0], state) || IsQuerySatisfiedHelper(query.children[1], state);
+        case NodeType_t::CompLess:
+        case NodeType_t::CompLessEq:
+        case NodeType_t::CompNeq:
+        case NodeType_t::CompEq:
+        case NodeType_t::CompGreater:
+        case NodeType_t::CompGreaterEq: {
+            std::string exprstring = ""; // This string can technically be precompiled.
+            query.tree_apply([&exprstring]( auto& node ){ exprstring += node.token; });
+            spdlog::debug("Assembled expression '{0}'", exprstring);
+            calculator c(exprstring.c_str());
+            return c.eval(state.GetSymbols()).asBool();
+        }
+        case NodeType_t::SubExpr:
+        case NodeType_t::Finally:
+        case NodeType_t::Globally:
+        case NodeType_t::Next:
+        case NodeType_t::Until:
+        case NodeType_t::Exists:
+        case NodeType_t::Forall:    return IsQuerySatisfiedHelper(query.children[0], state);
+        case NodeType_t::Negation:  return !IsQuerySatisfiedHelper(query.children[0], state);
+        // These are handled elsewhere, so they should output an error
+        case NodeType_t::Literal:
+        case NodeType_t::Var: // yes, even boolean valued variables are required to be on the lhs of an "== true" expression.
+        case NodeType_t::UNKNOWN:
+        default: spdlog::error("Something went wrong evaluating the query."); break;
+    }
+    return false;
+}
+
+bool ReachabilitySearcher::IsQuerySatisfied(Query& query, const TTA &state) {
+    if(query.root.type != NodeType_t::Exists) {
+        spdlog::critical("Only reachability queries are supported right now, sorry.");
+        return false;
+    }
+    return IsQuerySatisfiedHelper(query, state);
+}
---
+++ src/verifier/ReachabilitySearcher.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2020 Asger Gitz-Johansen
+
+   This file is part of mave.
+
+    mave is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    mave is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mave.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAVE_REACHABILITYSEARCHER_H
+#define MAVE_REACHABILITYSEARCHER_H
+#include <verifier/query_parsing/CTLQueryParser.h>
+#include <runtime/TTA.h>
+
+class ReachabilitySearcher {
+    static bool IsQuerySatisfied(Query& query, const TTA& state);
+};
+
+#endif //MAVE_REACHABILITYSEARCHER_H
