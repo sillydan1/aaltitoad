54523a6992ed1492a64bd3f8e4652ec627bc20c3
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-11-28 10:50:32 +0100

    Renamed everything from 'mave' to 'aaltitoad'

--- src/debug/aaltitoad_debug.h
+++ src/debug/aaltitoad_debug.h
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2020 Asger Gitz-Johansen
 
-   This file is part of mave.
+   This file is part of aaltitoad.
 
     mave is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
