e879d8df82da1b0211f2fff760a688bbd4fdbded
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-12-06 15:33:35 +0100

    Added Tree extensions file

---
+++ src/extensions/tree_extensions.cpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2020 Asger Gitz-Johansen
+
+   This file is part of aaltitoad.
+
+    aaltitoad is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    aaltitoad is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with aaltitoad.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "tree_extensions.h"
+
+std::string ConvertASTToString(const Tree<ASTNode>& tree) {
+    std::stringstream ss{};
+    tree.tree_apply([&ss](const ASTNode& n) { ss << n.token; });
+    return ss.str();
+}
