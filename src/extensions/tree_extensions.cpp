b5f088ceb9d75dfee6356e5a93982b51fea3e4d2
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-12-08 13:22:49 +0100

    Properly implemented the Tree to string conversion function

--- src/extensions/tree_extensions.cpp
+++ src/extensions/tree_extensions.cpp
@@ -18,8 +18,43 @@
  */
 #include "tree_extensions.h"
 
+void ConverterHelper(std::stringstream& ss, const Tree<ASTNode>& n) {
+    switch (n.root.type) {
+        case NodeType_t::Var:
+        case NodeType_t::Location:
+        case NodeType_t::Deadlock:
+        case NodeType_t::Literal:
+            ss << n.root.token; break;
+        case NodeType_t::LogicOr:
+        case NodeType_t::CompLess:
+        case NodeType_t::CompLessEq:
+        case NodeType_t::CompNeq:
+        case NodeType_t::CompEq:
+        case NodeType_t::CompGreater:
+        case NodeType_t::CompGreaterEq:
+        case NodeType_t::Operator:
+        case NodeType_t::LogicAnd:
+            ConverterHelper(ss, n.children[0]);
+            ss << " " << n.root.token << " ";
+            ConverterHelper(ss, n.children[1]);break;
+        case NodeType_t::SubExpr:
+            ss << "("; ConverterHelper(ss, n.children[0]); ss << ")";break;
+        case NodeType_t::Negation:
+        case NodeType_t::Finally:
+        case NodeType_t::Globally:
+        case NodeType_t::Next:
+        case NodeType_t::Until:
+        case NodeType_t::Exists:
+        case NodeType_t::Forall:
+            ss << n.root.token;
+            ConverterHelper(ss, n.children[0]);break;
+        case NodeType_t::UNKNOWN:
+        default: ss << n.root.token;break;
+    }
+}
+
 std::string ConvertASTToString(const Tree<ASTNode>& tree) {
     std::stringstream ss{};
-    tree.tree_apply([&ss](const ASTNode& n) { ss << n.token; });
+    ConverterHelper(ss, tree);
     return ss.str();
 }
