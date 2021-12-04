478ecbcce5a2b41f8986b33aec8a1164a840d6ff
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2021-12-04 22:23:40 +0100

    Patched a problem with the hashes...

--- src/verifier/ReachabilitySearcher.h
+++ src/verifier/ReachabilitySearcher.h
@@ -42,12 +42,12 @@ class ReachabilitySearcher {
 public:
     ReachabilitySearcher(const std::vector<const Query*>& queries, const TTA& initialState);
     inline bool Search(const nondeterminism_strategy_t& strategy = nondeterminism_strategy_t::PICK_FIRST) { return ForwardReachabilitySearch(strategy); }
-    void AddToWaitingList(const TTA& state, const std::vector<TTA::StateChange>& statechanges, bool justTocked);
+    void AddToWaitingList(const TTA& state, const std::vector<TTA::StateChange>& statechanges, bool justTocked, size_t state_hash);
 private:
     static bool IsSearchStateTockable(const SearchState& state);
     bool AreQueriesAnswered(const std::vector<QueryResultPair>& qres);
     bool IsQuerySatisfied(const Query& query, const TTA& state);
-    void AreQueriesSatisfied(std::vector<QueryResultPair>& queries, const TTA& state);
+    void AreQueriesSatisfied(std::vector<QueryResultPair>& queries, const TTA& state, size_t state_hash);
     bool ForwardReachabilitySearch(const nondeterminism_strategy_t& strategy);
     void PrintResults(const std::vector<QueryResultPair>& results);
     void OutputResults(const std::vector<QueryResultPair>& results);
