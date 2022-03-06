b62857afe1d714b7971b98c0ef839fd78901ba82
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-03-06 11:57:30 +0100

    Fixed a bug in edge.cpp

--- src/runtime/tta/edge.cpp
+++ src/runtime/tta/edge.cpp
@@ -11,7 +11,7 @@ auto edge_t::evaluate_updates(const symbol_table_t& environment) const -> symbol
 
 auto edge_t::is_satisfied(const symbol_table_t& environment) const -> bool {
     driver drv{environment};
-    auto res = drv.parse(updateExpression);
+    auto res = drv.parse(guardExpression);
     if(res)
         throw std::logic_error("Unable to evaluate update expression "+updateExpression);
     return std::get<bool>(drv.result["expression_result"]);
