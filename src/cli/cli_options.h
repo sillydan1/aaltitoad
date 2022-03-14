dba8361f6efbb2c160a1aa321003abe99e0ec927
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-03-14 07:22:53 +0100

    Fixed verbosity help message

--- src/cli/cli_options.h
+++ src/cli/cli_options.h
@@ -12,7 +12,7 @@ const option::Descriptor usage[] = {
                                                 "Options:" },
         {option::HELP,     0,"h", "help",option::Arg::None, "  --help, -h  \tPrint usage and exit." },
         {option::VERSION,0,"V", "version",option::Arg::None, "  --version, -V  \tPrint version and exit" },
-        {option::VERBOSITY,0,"", "verbosity",option::Arg::Optional, "  --verbosity, -v  \tSet verbosity level (--verbosity=4 (or -vvvv) for max verbosity)" },
+        {option::VERBOSITY,0,"", "verbosity",option::Arg::Optional, "  --verbosity, -v  \tSet verbosity level (--verbosity=6 (or -vvvvvv) for max verbosity)" },
         {option::VERBOSITY_SHORT,0,"v", "",option::Arg::None },
         {option::INPUT_FILEPATH,0,"i", "input",option::Arg::Optional, "  --input, -i      \tSet input filepath\n"
                                                               "                   \tExample: --input=path/to/model or -ipath/to/model" },
