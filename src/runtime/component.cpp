3e70253f60d2a64865224c765b65ad4e877d3b00
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-03-17 08:08:07 +0100

    debug

--- src/runtime/component.cpp
+++ src/runtime/component.cpp
@@ -12,7 +12,7 @@ auto component_t::get_enabled_edges(const symbol_table_t& environment) const -> 
 auto component_t::operator=(const std::string& new_location) -> component_t & {
     const auto& it = locations.find(new_location);
     if(it == locations.end())
-        throw std::logic_error("Location '"+new_location+"' is not a location in this component");
+        throw std::logic_error((std::stringstream{} << "Location '" << new_location << "' is not a location in this component").str().c_str());
     current_location = it->first;
     return *this;
 }
