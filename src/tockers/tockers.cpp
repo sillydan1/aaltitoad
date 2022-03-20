d7e91772429a921789b7b2b16809b620082ca482
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-03-20 17:57:47 +0100

    Started on the tocker interfaces

---
+++ src/tockers/tockers.cpp
@@ -0,0 +1,12 @@
+#include "tockers.h"
+
+void async_tocker_t::tock_async(const symbol_value_t& environment) {
+    job = std::async([this, &environment](){ return get_tock_values(environment); });
+}
+
+symbol_table_t async_tocker_t::tock(const symbol_value_t& environment) {
+    if(job.valid())
+        return job.get();
+    tock_async(environment);
+    return {};
+}
---
+++ src/tockers/tockers.h
@@ -0,0 +1,18 @@
+#ifndef AALTITOAD_ITOCKER_H
+#define AALTITOAD_ITOCKER_H
+#include <aaltitoadpch.h>
+#include <symbol_table.h>
+#include <future>
+
+struct tocker_t {
+    virtual symbol_table_t tock(const symbol_value_t& environment) = 0;
+};
+
+struct async_tocker_t : public tocker_t {
+    std::future<symbol_table_t> job{};
+    virtual symbol_table_t get_tock_values(const symbol_value_t& invocation_environment) = 0;
+    virtual void tock_async(const symbol_value_t& environment);
+    symbol_table_t tock(const symbol_value_t& environment) override;
+};
+
+#endif //AALTITOAD_ITOCKER_H
