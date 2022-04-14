1d017bab08845721a3f026a13c94148fa34f1341
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-04-14 20:23:01 +0200

    Added a bunch of tests
    And got confused. Investigating...

--- src/extensions/dfs.h
+++ src/extensions/dfs.h
@@ -11,7 +11,7 @@ template<typename T>
 using dfs_decorations_t = std::unordered_map<const association_node_t<T>*, dfs_decoration>;
 
 template<typename T>
-auto has_cycle_dfs(const association_graph<T>& g, const association_node_t<T>* n, dfs_decorations_t<T>& decorations) {
+auto has_cycle_dfs(const association_node_t<T>* n, dfs_decorations_t<T>& decorations) {
     if(decorations[n].finished)
         return false;
     if(decorations[n].visited)
@@ -19,7 +19,7 @@ auto has_cycle_dfs(const association_graph<T>& g, const association_node_t<T>* n
     decorations.at(n).visited = true;
 
     for(auto& neighbor : n->outgoing_edges) {
-        if(has_cycle_dfs(g, neighbor, decorations))
+        if(has_cycle_dfs<T>(neighbor, decorations))
             return true;
     }
 
@@ -31,7 +31,17 @@ template<typename T>
 auto has_cycle_dfs(const association_graph<T>& g) -> bool {
     dfs_decorations_t<T> decorations{};
     for(auto& node : g.get_nodes()) {
-        if(has_cycle_dfs(g, &node, decorations))
+        if(has_cycle_dfs<T>(&node, decorations))
+            return true;
+    }
+    return false;
+}
+
+template<typename T>
+auto has_cycle_dfs(const scc_t<T>& component) -> bool {
+    dfs_decorations_t<T> decorations{};
+    for(auto& node : component) {
+        if(has_cycle_dfs<T>(node, decorations))
             return true;
     }
     return false;
--- test/algorithms/dfs_tests.cpp
+++ test/algorithms/dfs_tests.cpp
@@ -14,3 +14,73 @@ TEST_CASE("givenAcyclicGraph_whenCheckingForCycles_thenReturnFalse") {
     my_graph.insert_edge(0, 4);
     REQUIRE(!has_cycle_dfs(my_graph));
 }
+
+TEST_CASE("givenGraphWithACyclicSCC_whenCheckingSCCForCycles_thenReturnTrue") {
+    association_graph<std::string> my_graph{{{"L0"}, {"L1"}, {"L2"}}};
+    my_graph.insert_edge(0, 1);
+    my_graph.insert_edge(1, 2);
+    my_graph.insert_edge(2, 0);
+    auto sccs = tarjan(my_graph);
+    REQUIRE(sccs.size() == 1);
+    for(auto& scc : sccs)
+        REQUIRE(has_cycle_dfs<std::string>(scc));
+}
+
+TEST_CASE("givenGraphWithAnAcyclicSCC_whenCheckingSCCForCycles_thenReturnFalse") {
+    association_graph<std::string> my_graph{{{"L0"}, {"L1"}, {"L2"}}};
+    my_graph.insert_edge(0, 1);
+    my_graph.insert_edge(1, 2);
+    auto sccs = tarjan(my_graph);
+    REQUIRE(sccs.size() == 1);
+    for(auto& scc : sccs)
+        REQUIRE(!has_cycle_dfs<std::string>(scc));
+}
+
+TEST_CASE("givenGraphWithACyclicAndAcyclicSCCs_whenCheckingSCCForCycles_thenReturnTrueThenFalse") {
+    association_graph<std::string> my_graph{{{"L0"}, {"L1"}, {"L2"}, {"L3"}}};
+    my_graph.insert_edge(0, 2);
+    my_graph.insert_edge(3, 2);
+    my_graph.insert_edge(1, 2);
+    my_graph.insert_edge(2, 2);
+    auto sccs = tarjan(my_graph);
+    for(auto& scc : sccs) {
+        std::cout << "{ ";
+        for(auto& n : scc)
+            std::cout << n->data << " ";
+        std::cout << "} (cyclic: " << std::boolalpha << has_cycle_dfs<std::string>(scc) << ")" << std::endl;
+    }
+}
+
+TEST_CASE("toyExampleOfCyclicAndNonCyclicSCCs", "[.]") {
+    association_graph<std::string> my_graph{{
+        {"L0"}, {"L1"}, {"L2"}, {"L3"},
+        {"L4"}, {"L5"}, {"L6"}, {"L7"}
+    }};
+    // SCC 1
+    my_graph.insert_edge(0, 4);
+    my_graph.insert_edge(4, 1);
+    my_graph.insert_edge(1, 0);
+    // SCC 2
+    my_graph.insert_edge(5, 6);
+    my_graph.insert_edge(6, 5);
+    // SCC 3
+    my_graph.insert_edge(2, 3);
+    my_graph.insert_edge(3, 2);
+    // SCC 4
+    //my_graph.insert_edge(7, 7);
+    // Extras
+    my_graph.insert_edge(7, 6);
+    my_graph.insert_edge(7, 3);
+    my_graph.insert_edge(6, 2);
+    my_graph.insert_edge(5, 4);
+    my_graph.insert_edge(5, 1);
+    my_graph.insert_edge(2, 1);
+
+    auto sccs = tarjan(my_graph);
+    for(auto& scc : sccs) {
+        std::cout << "{ ";
+        for(auto& n : scc)
+            std::cout << n->data << " ";
+        std::cout << "} (cyclic: " << std::boolalpha << has_cycle_dfs<std::string>(scc) << ")" << std::endl;
+    }
+}
