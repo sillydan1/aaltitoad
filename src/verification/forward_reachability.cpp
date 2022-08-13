59286c7da5130e1c8251eae15a5184245a83dd12
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2022-08-13 15:53:21 +0200

    wip

--- src/verification/forward_reachability.cpp
+++ src/verification/forward_reachability.cpp
@@ -1,5 +1,8 @@
 #include "forward_reachability.h"
 
 namespace aaltitoad {
+    auto forward_reachability_searcher::is_reachable(const aaltitoad::ntta_t &model, const ctl::query &q) -> bool {
 
+        return false;
+    }
 }
--- src/verification/forward_reachability.h
+++ src/verification/forward_reachability.h
@@ -1,12 +1,12 @@
 #ifndef AALTITOAD_FORWARD_REACHABILITY_H
 #define AALTITOAD_FORWARD_REACHABILITY_H
 #include "ctl.h"
+#include "ntta/tta.h"
 
 namespace aaltitoad {
     class forward_reachability_searcher {
-
     public:
-        
+        auto is_reachable(const ntta_t& model, const ctl::query& q) -> bool;
     };
 }
 
