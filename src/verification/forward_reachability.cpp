6d5a3059d53cfa391d64fc6adfabd45efadb0caa
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2022-08-26 07:35:41 +0200

    chore: Cleanup todos in forward_reachability.cpp

--- src/verification/forward_reachability.cpp
+++ src/verification/forward_reachability.cpp
@@ -2,16 +2,20 @@
 #include "ctl_sat.h"
 
 namespace aaltitoad {
+    // TODO: <feature request> todo: add a new issue on github
+    //       Max search time? / max explosion allowed? - could be done with sizeof(s0) / GiB/MiB
+    //       we could do a prompt: "reached RAM limit with xxx states remaining, continue?"
+    //       With that, we could also do a Waiting-list velocity calculation. Maybe even output
+    //       waiting-list size to a CSV file so it could be graphed! (very useful for papers)
+    //       This behavior can be embedded into a specialization of colliding_multimap<T>
     auto forward_reachability_searcher::is_reachable(const aaltitoad::ntta_t& s0, const ctl::compiler::compiled_expr_t& q) -> bool {
-        // TODO: List of queries
         // TODO: statistics on all return statements (info)
-        // TODO: Catch SIGTERM's and write statistics (info)
+        // TODO: Catch SIGTERM's (ctrl-c) and write statistics (info)
         // TODO: Periodically print waiting list size for debugging purposes (debug)
-        // TODO: Max search time? / max explosion allowed? nah
         W = {s0}; P = {};
         while(!W.empty()) {
             auto s = W.pop();
-            if(is_satisfied(q, s))
+            if(is_satisfied(q, s)) // TODO: List of queries
                 return true; // TODO: return traceable results
             for(auto& si : s.tick()) {
                 auto successor = s + si;
