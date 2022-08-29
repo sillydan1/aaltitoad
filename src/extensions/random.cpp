4fe512415f5e40db0d73b2a203bbe3c2e7b7b5b1
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-08-29 17:49:26 +0200

    fix: use the correct uniform distribution in random.cpp

--- src/extensions/random.cpp
+++ src/extensions/random.cpp
@@ -5,12 +5,12 @@ namespace aaltitoad::random {
     std::random_device r;
     auto value(int min, int max) -> int {
         std::default_random_engine e1(r());
-        return std::uniform_int_distribution<size_t>(min,max)(e1);
+        return std::uniform_int_distribution<int>(min,max)(e1);
     }
 
     auto value(double min, double max) -> double {
         std::default_random_engine e1(r());
-        return std::uniform_int_distribution<size_t>(min, max)(e1);
+        return std::uniform_real_distribution<double>(min, max)(e1);
     }
 }
 
