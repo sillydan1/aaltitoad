fbd4293eb1d77f14db42d1e117df1b493ee5aac4
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-12-02 06:57:59 +0100

    revert: changes to tta_builder

--- src/ntta/builder/ntta_builder.cpp
+++ src/ntta/builder/ntta_builder.cpp
@@ -1,60 +1,50 @@
 #include <util/exceptions/parse_error.h>
-#include <drivers/tree_compiler.h>
 #include "ntta_builder.h"
 
 namespace aaltitoad {
-    tta_builder::tta_builder(const expr::symbol_table_tree_t::iterator& symbols, const expr::symbol_table_tree_t::iterator& external_symbols)
-     : factory{}, internal_scope{symbols}, external_scope{external_symbols}, empty_guard{}, starting_location{}
+    tta_builder::tta_builder(expr::symbol_table_t& symbols, expr::symbol_table_t& external_symbols)
+     : symbols{symbols + external_symbols}, factory{}, empty_guard{}, starting_location{}
     {
         empty_guard = compile_guard("");
     }
-
     auto tta_builder::get_name() -> std::optional<std::string> {
         return tta_name;
     }
-
     auto tta_builder::set_name(const std::string& name) -> tta_builder& {
         tta_name = name;
         return *this;
     }
-
     auto tta_builder::set_starting_location(const std::string& name) -> tta_builder& {
         starting_location = name;
         return *this;
     }
-
     auto tta_builder::add_location(const std::string& name) -> tta_builder& {
         factory.add_node({name});
         return *this;
     }
-
     auto tta_builder::add_locations(const std::vector<std::string>& names) -> tta_builder& {
         for(auto& name : names)
             add_location(name);
         return *this;
     }
-
     auto tta_builder::add_edge(const edge_construction_element& e) -> tta_builder& {
         factory.add_edge(e.source, e.target, {.guard=compile_guard(e.guard), .updates=compile_update(e.update)});
         return *this;
     }
-
     auto tta_builder::add_edges(const std::vector<edge_construction_element>& es) -> tta_builder& {
         for(auto& e : es)
             add_edge(e);
         return *this;
     }
-
     auto tta_builder::build() -> tta_t {
         if(!starting_location.has_value())
             throw std::logic_error("no starting location provided");
         return {std::move(factory.build_heap()), starting_location.value()};
     }
-
     auto tta_builder::compile_guard(const std::optional<std::string>& guard) -> expr::compiler::compiled_expr_t {
         if(!guard.has_value())
             return empty_guard;
-        expr::tree_compiler compiler{internal_scope};
+        expr::compiler compiler{symbols};
         auto res = compiler.parse(guard.value());
         if(res != 0)
             throw std::logic_error(compiler.error);
@@ -63,7 +53,7 @@ namespace aaltitoad {
     auto tta_builder::compile_update(const std::optional<std::string>& update) -> expr::compiler::compiled_expr_collection_t {
         if(!update.has_value())
             return {};
-        expr::tree_compiler compiler{internal_scope};
+        expr::compiler compiler{symbols};
         auto res = compiler.parse(update.value());
         if(res != 0)
             throw std::logic_error(compiler.error);
