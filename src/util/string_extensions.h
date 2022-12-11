b4408e15ac7dde337639da6ffb3fcf43b01c0ba3
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-12-11 13:00:09 +0100

    fix: add license info to all source files

--- src/util/string_extensions.h
+++ src/util/string_extensions.h
@@ -1,20 +1,19 @@
 /**
- * Copyright (C) 2020 Asger Gitz-Johansen
+ * aaltitoad - a verification engine for tick tock automata models
+   Copyright (C) 2023 Asger Gitz-Johansen
 
-   This file is part of aaltitoad.
-
-    aaltitoad is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    aaltitoad is distributed in the hope that it will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with aaltitoad.  If not, see <https://www.gnu.org/licenses/>.
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef AALTITOAD_STRINGEXTENSIONS_H
 #define AALTITOAD_STRINGEXTENSIONS_H
