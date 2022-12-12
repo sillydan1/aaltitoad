2a8220192c0c97d32e83bd210e4c5e57448cede8
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-12-12 11:27:24 +0100

    test: add edge-case test suite

--- test/verification/parser_tests.cpp
+++ test/verification/parser_tests.cpp
@@ -17,6 +17,8 @@
  */
 #include <catch2/catch_test_macros.hpp>
 #include <parser/hawk/hawk_parser.h>
+#include <util/exceptions/parse_error.h>
+
 #ifndef AALTITOAD_PROJECT_DIR
 #define AALTITOAD_PROJECT_DIR "."
 #endif
@@ -25,7 +27,7 @@ SCENARIO("parsing fischer-n suite", "[hawk_parser]") {
     spdlog::set_level(spdlog::level::trace);
     std::vector<std::string> folders{}, ignore_list{".*\\.ignore\\.txt"};
     GIVEN("the fischer-2 test set") {
-        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/fischer-2");
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/fischer-suite/fischer-2");
         WHEN("parsing the network") {
             std::unique_ptr<aaltitoad::ntta_t> n{aaltitoad::hawk::load(folders, ignore_list)};
             std::cout << *n << std::endl;
@@ -35,7 +37,7 @@ SCENARIO("parsing fischer-n suite", "[hawk_parser]") {
         }
     }
     GIVEN("the fischer-5 test set") {
-        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/fischer-5");
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/fischer-suite/fischer-5");
         WHEN("parsing the network") {
             std::unique_ptr<aaltitoad::ntta_t> n{aaltitoad::hawk::load(folders, ignore_list)};
             std::cout << *n << std::endl;
@@ -45,13 +47,66 @@ SCENARIO("parsing fischer-n suite", "[hawk_parser]") {
         }
     }
     GIVEN("the fischer-10 test set") {
-        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/fischer-10");
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/fischer-suite/fischer-10");
         WHEN("parsing the network") {
             std::unique_ptr<aaltitoad::ntta_t> n{aaltitoad::hawk::load(folders, ignore_list)};
             std::cout << *n << std::endl;
             THEN("eleven TTAs are constructed (fischer instances + main)") {
                 REQUIRE(n->components.size() == 11);
             }
+        }
+    }
+}
+
+SCENARIO("parsing failing suite", "[hawk_parser]") {
+    spdlog::set_level(spdlog::level::trace);
+    std::vector<std::string> folders{}, ignore_list{".*\\.ignore\\.txt"};
+    GIVEN("bad-template-params") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-template-params");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-invocation-args") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-invocation-args");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-invocation-args-amount") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-invocation-args-amount");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-declarations") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-declarations");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-template-name") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-template-name");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-duplicated-locations") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-duplicated-locations");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-recursive-instantiation") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-recursive-instantiation");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS_AS(aaltitoad::hawk::load(folders, ignore_list), std::logic_error);
+        }
+    }
+    GIVEN("bad-hawk-project") {
+        folders.emplace_back(AALTITOAD_PROJECT_DIR "/test/verification/failing-suite/bad-hawk-project");
+        THEN("parsing the network fails with a parse error") {
+            REQUIRE_THROWS(aaltitoad::hawk::load(folders, ignore_list));
         }
     }
 }
