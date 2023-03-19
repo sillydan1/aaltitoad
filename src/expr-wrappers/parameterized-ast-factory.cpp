6ff57f503b7f886979bb59591bd2ffc51a5e5279
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-03-19 17:00:49 +0100

    fix: parameterization of partial identifiers in expressions

--- src/expr-wrappers/parameterized-ast-factory.cpp
+++ src/expr-wrappers/parameterized-ast-factory.cpp
@@ -6,6 +6,20 @@ namespace aaltitoad {
     : scope_prefix{scope_prefix}, parameter_arguments{paramargs}, local_identifiers{local_names} {}
 
     parameterized_ast_factory::~parameterized_ast_factory() {}
+    
+    auto parameterized_ast_factory::get_parameterized_identifier(const std::string& identifier) const -> std::string {
+        auto id = trim_copy(identifier);
+        for(auto& parameter : parameter_arguments) {
+            std::regex r{"\\." + parameter.first + "$"};
+            std::smatch match;
+            if (std::regex_search(id.cbegin(), id.cend(), match, r)) {
+                auto replace = "." + expr::as_string(parameter.second);
+                auto parameterized_identifier = std::regex_replace(id, r, replace);
+                return parameterized_identifier;
+            }
+        }
+        return id;
+    }
 
     auto parameterized_ast_factory::build_identifier(const std::string& identifier) -> expr::syntax_tree_t {
         // Is the identifier a parameter name?
@@ -14,8 +28,8 @@ namespace aaltitoad {
         // Is the identifier a locally declared variable?
         if(std::find(local_identifiers.begin(), local_identifiers.end(), identifier) != local_identifiers.end())
             return expr::ast_factory::build_identifier(scope_prefix + identifier);
-        // Otherwise, default case
-        return expr::ast_factory::build_identifier(identifier);
+        // Otherwise, default case (parameterized, just in case it is partial)
+        return expr::ast_factory::build_identifier(get_parameterized_identifier(identifier));
     }
 }
 
