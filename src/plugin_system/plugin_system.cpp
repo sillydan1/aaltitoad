c2dcc30b2ff5353e6a2a15ee18405a22c5097d09
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-10-17 19:06:40 +0200

    cleanup: some minor code cleanup

--- src/plugin_system/plugin_system.cpp
+++ src/plugin_system/plugin_system.cpp
@@ -23,7 +23,7 @@ namespace aaltitoad::plugins {
     template<typename T>
     T load_symbol(void* handle, const std::string& symbol_name) {
         dlerror(); // clear errors
-        T val = (T) dlsym(handle, symbol_name.c_str());
+        auto val = (T) dlsym(handle, symbol_name.c_str());
         auto* err = dlerror();
         if(!val || err) {
             dlclose(handle);
@@ -33,9 +33,9 @@ namespace aaltitoad::plugins {
     }
 
     bool is_dynamic_library(const std::string& filename) {
-        return contains(filename, ".so")
-               || contains(filename, ".dll")
-               || contains(filename, ".dylib");
+        return contains(filename, ".so") ||
+               contains(filename, ".dll") ||
+               contains(filename, ".dylib");
     }
 
     plugin_map_t load(const std::vector<std::string> &search_directories) {
@@ -48,36 +48,33 @@ namespace aaltitoad::plugins {
             spdlog::trace("searching for plugins in: {0}", directory);
             for (const auto &entry: std::filesystem::directory_iterator(directory)) {
                 try {
-                    if (entry.is_regular_file()) {
-                        if (is_dynamic_library(entry.path().filename())) {
-                            spdlog::trace("attempting to load file '{0}' as a plugin",
-                                          entry.path().filename().string());
-                            std::string entry_name = entry.path().c_str();
-                            auto *handle = dlopen(entry.path().c_str(), RTLD_LAZY);
-                            if (!handle)
-                                throw std::logic_error("could not load as a shared/dynamic library");
-                            auto stem = std::string(load_symbol<get_plugin_name_t>(handle, "get_plugin_name")());
-                            auto type = static_cast<plugin_type>(load_symbol<get_plugin_type_t>(handle,
-                                                                                                "get_plugin_type")());
-                            auto version = std::string(load_symbol<get_plugin_version_t>(handle, "get_plugin_version")());
-                            if (loaded_plugins.contains(stem))
-                                throw std::logic_error("plugin with name '" + stem +
-                                                       "' is already loaded. All plugins must have unique names");
-                            switch (type) {
-                                case plugin_type::tocker: {
-                                    auto ctor = load_symbol<tocker_ctor_t>(handle, "create_tocker");
-                                    loaded_plugins.insert(std::make_pair(stem, plugin_t{type, version, ctor}));
-                                    break;
-                                }
-                                case plugin_type::parser: {
-                                    auto load = load_symbol<parser_func_t>(handle, "load");
-                                    loaded_plugins.insert(std::make_pair<>(stem, plugin_t{type, version, load}));
-                                    break;
-                                }
-                            }
-                            spdlog::debug("loaded plugin '{0}'", stem);
+                    if (!entry.is_regular_file())
+                        continue;
+                    if (!is_dynamic_library(entry.path().filename()))
+                        continue;
+                    spdlog::trace("attempting to load file '{0}' as a plugin", entry.path().filename().string());
+                    std::string entry_name = entry.path().c_str();
+                    auto* handle = dlopen(entry.path().c_str(), RTLD_LAZY);
+                    if (!handle)
+                        throw std::logic_error("could not load as a shared/dynamic library");
+                    auto stem = std::string(load_symbol<get_plugin_name_t>(handle, "get_plugin_name")());
+                    auto type = static_cast<plugin_type>(load_symbol<get_plugin_type_t>(handle, "get_plugin_type")());
+                    auto version = std::string(load_symbol<get_plugin_version_t>(handle, "get_plugin_version")());
+                    if (loaded_plugins.contains(stem))
+                        throw std::logic_error("plugin with name '" + stem + "' is already loaded. All plugins must have unique names");
+                    switch (type) {
+                        case plugin_type::tocker: {
+                            auto ctor = load_symbol<tocker_ctor_t>(handle, "create_tocker");
+                            loaded_plugins.insert(std::make_pair(stem, plugin_t{type, version, ctor}));
+                            break;
+                        }
+                        case plugin_type::parser: {
+                            auto load = load_symbol<parser_func_t>(handle, "load");
+                            loaded_plugins.insert(std::make_pair<>(stem, plugin_t{type, version, load}));
+                            break;
                         }
                     }
+                    spdlog::debug("loaded plugin '{0}'", stem);
                 } catch (std::exception &e) {
                     aaltitoad::warnings::warn(plugin_load_failed, "failed to load '"+entry.path().string()+"' as a plugin: "+e.what());
                 }
