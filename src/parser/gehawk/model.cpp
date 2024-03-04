95329cda2c2e9bc89de18abf77cdd441446efb3f
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2024-03-04 21:04:22 +0100

    wip

---
+++ src/parser/gehawk/model.cpp
@@ -0,0 +1,23 @@
+#include "model.h"
+
+namespace aaltitoad::gehawk::model {
+    void from_json(const nlohmann::json& j, location_t& t) {
+
+    }
+
+    void from_json(const nlohmann::json& j, nail_t& t) {
+
+    }
+
+    void from_json(const nlohmann::json& j, edge_t& t) {
+
+    }
+
+    void from_json(const nlohmann::json& j, tta_instantiation_t& t) {
+
+    }
+
+    void from_json(const nlohmann::json& j, tta_t& t) {
+
+    }
+}
--- src/parser/gehawk/model.h
+++ src/parser/gehawk/model.h
@@ -1,5 +1,9 @@
 #ifndef GEHAWK_MODEL_H
 #define GEHAWK_MODEL_H
+#include <optional>
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
 
 namespace aaltitoad::gehawk::model {
     enum class location_type_t {
@@ -15,15 +19,47 @@ namespace aaltitoad::gehawk::model {
         invalid=-1
     };
 
+    enum class nail_type_t {
+        guard=0,
+        update,
+        comment,
+        invalid=-1,
+    };
+
     struct location_t {
         location_type_t type;
-        std::string nickname;
+        std::optional<std::string> nickname;
         immediacy_t immediacy;
     };
 
-    struct tta_template_t {
-
+    struct nail_t {
+        nail_type_t type;
+        std::string expression;
+        std::optional<std::string> nickname;
     };
+
+    struct tta_instantiation_t {
+        std::string template_name;
+    };
+
+    struct edge_t {
+        std::string source;
+        std::string target;
+    };
+
+    struct tta_t {
+        std::string name;
+        std::vector<location_t> locations;
+        std::vector<nail_t> nails;
+        std::vector<edge_t> edges;
+        std::vector<tta_instantiation_t> instantiations;
+    };
+
+    void from_json(const nlohmann::json& j, location_t& t);
+    void from_json(const nlohmann::json& j, nail_t& t);
+    void from_json(const nlohmann::json& j, edge_t& t);
+    void from_json(const nlohmann::json& j, tta_instantiation_t& t);
+    void from_json(const nlohmann::json& j, tta_t& t);
 }
 
 #endif // !GEHAWK_MODEL_H
